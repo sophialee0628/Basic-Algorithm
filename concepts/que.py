59b4dad7572f4d03a4e3f528418294062f0c40bf
Sophia <lsemd96@gmail.com>
2019-03-29 16:11:40 +0900

    first commit

--- concepts/que.py
+++ concepts/que.py
@@ -6,3 +6,17 @@ def put(item):
 def get():
     return queue.pop()
 
+if __name__=='__main__':
+    queue=[]
+    put(1)
+    put(2)
+    put(3)
+    put(4)
+
+    print("현재 que 모습")
+    print(queue)
+
+    while queue:
+        print("POP > {}".format(get()))
+
+
